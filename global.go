53b1f2cadba7f509830bc96635a9465dfeeb91f1
costa92 <944310536@qq.com>
2024-02-24 22:38:15 +0800

    fix: add log

--- global.go
+++ global.go
@@ -2,31 +2,4 @@ package logger
 
 var (
 	defaultConfig = NewOptions()
-	//l             = NewLogger(defaultConfig)
 )
-
-//func SetConfig(config *Options) {
-//	l = newLogger(config)
-//}
-//
-//func SetLevel(level Level) {
-//	l.SetLevel(level)
-//}
-
-//func GetLevel() Level {
-//	return l.Level()
-//}
-
-//func SetOutputPaths(outputPaths []string) {
-//	l.config.OutputPaths = outputPaths
-//	l = newLogger(l.config)
-//}
-//
-//func NewLogger(opt *Config) Logger {
-//	l := newLogger(config)
-//	return l
-//}
-//
-//func GetLogger() Logger {
-//	return l
-//}
